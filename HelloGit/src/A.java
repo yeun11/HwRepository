68b3200246bda1ef546e575ac7d847c887f4b427
YeEun <YeEun@192.168.219.134>
2022-06-22 16:05:59 +0900

    First commit in devC branch

--- HelloGit/src/A.java
+++ HelloGit/src/A.java
@@ -3,7 +3,8 @@ public class A {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Git A!");
-
+		C ctest = new C();  //devC
+		ctest.testC();      //devC
 	}
 
 }
--- HelloGit/src/C.java
+++ HelloGit/src/C.java
@@ -2,5 +2,6 @@
 public class C {
 	public void testC() {
 		System.out.println("Hello Git C!");
+		System.out.println("Here is devC branch");
 	}
 }
